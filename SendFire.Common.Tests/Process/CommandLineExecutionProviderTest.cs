5624e3642f7d112f0c33df0ea3aece8d735375cd
Eun Eby <e.keum1205@gmail.com>
2017-12-18 16:45:32 -0600

    Added a timout parameter on CommandLineExecutionProvider. So if the the timeout parameter passed, the process took more than it, the process is being killed.

--- SendFire.Common.Tests/Process/CommandLineExecutionProviderTest.cs
+++ SendFire.Common.Tests/Process/CommandLineExecutionProviderTest.cs
@@ -1,4 +1,5 @@
 ﻿using SendFire.Common.Process;
+using System;
 using System.Collections.Generic;
 using Xunit;
 namespace SendFire.Common.Tests.Process
@@ -8,16 +9,15 @@ namespace SendFire.Common.Tests.Process
         public static string today = System.DateTime.Now.ToShortDateString();
         [Theory,
             MemberData(nameof(SystemCommandArguments))]
-        public void ProcessCommand(string[] commands, bool runAsBatch)
+        public void ProcessCommandAsBatch(string[] commands, bool runAsBatch)
         {
             var commandLineExecution = new CommandLineExecutionProvider();
             var output = commandLineExecution.ProcessCommands(commands, runAsBatch);
             Assert.NotEmpty(output);
-          //  Assert.Contains("JAVAHOME", output);
         }
         [Theory,
            InlineData(new string[] { "echo %HOME%", "set JAVAHOME=\"C:\\Temp\"", "echo %JAVAHOME%" }, true)]
-        public void ProcessSysCommand(string[] commands, bool runAsBatch)
+        public void ProcessCommandAsBatchForSysEnvironment(string[] commands, bool runAsBatch)
         {
             var commandLineExecution = new CommandLineExecutionProvider();
             var output = commandLineExecution.ProcessCommands(commands, runAsBatch);
@@ -27,7 +27,7 @@ namespace SendFire.Common.Tests.Process
         [Theory,
            InlineData(new string[] { "mkdir c:\\temp3" ,"echo eun555" },true),
            InlineData(new string[] { "mkdir c:\\temp5","cd c:\\temp5", "mkdir eun555", "dir" }, true)]
-        public void ProcessSysCommand2(string[] commands, bool runAsBatch)
+        public void ProcessCommandAsBatch2(string[] commands, bool runAsBatch)
         {
             var commandLineExecution = new CommandLineExecutionProvider();
             var output = commandLineExecution.ProcessCommands(commands, runAsBatch);
@@ -36,7 +36,7 @@ namespace SendFire.Common.Tests.Process
         }
         [Theory,
            InlineData(new string[] { "cmd /c dir c:\\" }, false)]
-        public void ProcessMultipleCommands(string[] commands, bool runAsBatch)
+        public void ProcessCommands(string[] commands, bool runAsBatch)
         {
             var commandLineExecution = new CommandLineExecutionProvider();
             var output = commandLineExecution.ProcessCommands(commands, runAsBatch);
@@ -45,15 +45,33 @@ namespace SendFire.Common.Tests.Process
          }
         [Theory,
         InlineData(new string[] { "cmd /c cd c:\\temp" ,"cmd /c mkdir c:\\temp\\blob", "cmd /c dir c:\\temp" }, false)]
-        public void ProcessMultipleCommands2(string[] commands, bool runAsBatch)
+        public void ProcessCommands2(string[] commands, bool runAsBatch)
         {
+            var timeoutMs = 30000;
             var commandLineExecution = new CommandLineExecutionProvider();
-            var output = commandLineExecution.ProcessCommands(commands, runAsBatch);
+            var output = commandLineExecution.ProcessCommands(commands, runAsBatch, timeoutMs);
             Assert.NotEmpty(output);
             Assert.Contains("temp",output);
             Assert.Contains("blob", output);
         }
-
+        [Theory,
+          InlineData(new string[] { "cd c:\\temp", "pause", "dir c:\\temp" }, true)]
+        public void ProcessCommandAsBatchForTimeoutTest(string[] commands, bool runAsBatch)
+        {
+            var commandLineExecution = new CommandLineExecutionProvider();
+            var timeoutMs = 1000;
+            Exception ex =Assert.Throws<TimeoutException>(()=> commandLineExecution.ProcessCommands(commands, runAsBatch, timeoutMs));
+            Assert.Equal($"Process did not finish in {timeoutMs} ms.", ex.Message);
+        }
+        [Theory,
+          InlineData(new string[] { "cmd /c cd c:\\temp", "cmd pause" }, false)]
+        public void ProcessCommandForTimeoutTest(string[] commands, bool runAsBatch)
+        {
+            var commandLineExecution = new CommandLineExecutionProvider();
+            var timeoutMs = 5000;
+            Exception ex = Assert.Throws<TimeoutException>(() => commandLineExecution.ProcessCommands(commands, runAsBatch, timeoutMs));
+            Assert.Equal($"Process did not finish in {timeoutMs} ms.", ex.Message);
+        }
         public static IEnumerable<object[]> SystemCommandArguments()
         {
             var systemCommandArgument =new[]
