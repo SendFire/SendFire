5507b04888cd3f469855b8a325a106a71f35ba5d
James Eby <temporafugiunt@gmail.com>
2017-12-19 11:55:03 -0600

    Got sendfire server running using default connection string.

--- SendFire.Agent/AgentServiceFactory.cs
+++ SendFire.Agent/AgentServiceFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Hangfire;
 using Hangfire.SqlServer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SendFire.Service.BaseClasses;
 using SendFire.Service.Interfaces;
@@ -15,7 +16,8 @@ namespace SendFire.Agent
         {
             //services.AddSingleton<ISendFireService, AgentService>();
             //var sendFireDb =@"Server=.\\sqlexpress; Database=SendFire; User Id=SendFire; Password=SendFire2017#!";
-            GlobalConfiguration.Configuration.UseSqlServerStorage("SendFireDB");
+            var connectionString = Configuration.GetConnectionString("SendFireDB");
+            GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
             //services.AddHangfire(x => x.UseSqlServerStorage("SendFireDB"));
             //GlobalConfiguration.Configuration.UseSqlServerStorage(sendFireDB);
             //JobStorage.Current = new SqlServerStorage(sendFireDB);
