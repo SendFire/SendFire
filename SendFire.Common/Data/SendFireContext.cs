ab478fefa47d0acdbfd05f764f0c5beb78d065ab
Adam Tumminaro <adam@tumminaro.com>
2017-12-18 20:47:50 -0600

    Added EF and ServerQueue Table

---
+++ SendFire.Common/Data/SendFireContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SendFire.Common.Data.Models;
+
+namespace SendFire.Common.Data
+{
+    public class SendFireContext : DbContext
+    {
+        public SendFireContext(DbContextOptions<SendFireContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("SendFire");
+        }
+
+        public DbSet<ServerQueue> ServerQueues { get; set; }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<SendFireContext>
+    {
+        public SendFireContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<SendFireContext>();
+            var connectionString = configuration.GetConnectionString("SendFireDB");
+            builder.UseSqlServer(connectionString);
+            return new SendFireContext(builder.Options);
+        }
+    }
+}
