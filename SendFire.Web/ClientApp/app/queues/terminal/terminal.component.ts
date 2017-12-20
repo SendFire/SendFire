d5123f4f1a70391765897194bdbe5c60e479f965
Adam Tumminaro <adam@spaceshipb.com>
2017-12-20 17:42:42 -0600

    not typing anymore, but still allowing the option

--- SendFire.Web/ClientApp/app/queues/terminal/terminal.component.ts
+++ SendFire.Web/ClientApp/app/queues/terminal/terminal.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import Typed from 'typed.js';
 import { JobService } from '../../services/job.service';
 
-function scrollToBottom(id) {
-  const div = document.getElementById(id);
-  if(!div) { return; }
-  div.scrollTop = div.scrollHeight - div.clientHeight;
-}
+
 
 @Component({
   selector: 'sf-terminal',
@@ -14,29 +10,43 @@ function scrollToBottom(id) {
   styleUrls: ['./terminal.component.scss']
 })
 export class TerminalComponent implements OnInit {
-  private _messages:string[] = [];
-  private messageList:string[] = [];
-  private _id:string;
-  private _scrollInterval:any;
-  @Input() 
+  private typeOut:boolean = false;
+  private _output: string[] = [];
+  private _messages: string[] = [];
+  private messageList: string[] = [];
+  private _id: string;
+  private _scrollInterval: any;
+  @Input()
   set id(passedId) {
     this._id = passedId;
+    if(!this._id) {return;}
     this.fetchResult();
   };
 
-  constructor(private _jobService: JobService){}
+  constructor(private _jobService: JobService) { }
 
   ngOnInit() {
-  }
 
-  startInterval(){
+  }
+  
+  scrollToBottom() {
+    var elem = document.getElementById('main-term');
+    if (!elem) { return; }
+    elem.scrollTop = elem.scrollHeight;
+  }
+  startInterval() {
 
     this._scrollInterval = setInterval(() => {
-       var elem = document.getElementById('main-term');
-       if(!elem) {return;}
-       elem.scrollTop = elem.scrollHeight;
-      }, 500);
+      this.scrollToBottom();
+    }, 45);
 
+  }
+
+  addProcessing() {
+    let processing = this._messages[0];
+    if (!processing) {
+      this.addMessage("Running...");
+    }
   }
 
   addMessage(msg) {
@@ -46,11 +56,24 @@ export class TerminalComponent implements OnInit {
 
   updateTerminal() {
     var output = this._messages[this._messages.length - 1];
+    var split = output.split("<br />");
+    if(this.typeOut) {
+      this.updateTerminalTyped(output);
+    } else {
+      this.updateTerminalNoTyped(split);
+    }
+
+
+  }
+
+  updateTerminalTyped(output) {
+    this.startInterval()
     setTimeout(() => {
       var d = document.getElementById(`msg${this._messages.length}`);
-      if(!d) {
+      if (!d) {
         return;
       }
+
       const typd = new Typed(`#msg${this._messages.length}`, {
         strings: [output],
         typeSpeed: 0,
@@ -62,34 +85,46 @@ export class TerminalComponent implements OnInit {
           setTimeout(() => {
             clearInterval(this._scrollInterval);
           }, 900);
-          
+
         },
       });
-
-      this.startInterval();
     }, 500);
+  }
 
+  updateTerminalNoTyped(split) {
     
+    this.startInterval();
+    split.forEach((s, i) => {
+      setTimeout(() => {
+        this._output.push(s)
+        if (i === split.length - 1) {
+          setTimeout(() => {
+            clearInterval(this._scrollInterval);
+          }, 200)
+        }
+      }, 50 * i);
+    })
   }
 
   fetchResult() {
+    
     this._jobService.getResults(this._id).subscribe(results => {
-        if(!results || !results.results || !results.results.Result) {
-          this.addMessage('Processing');
-          this.updateTerminal();
-          setTimeout(() => { this.fetchResult() }, 5000);
-          return;
-        }
-        const result = results.results.Result.substr(1).slice(0, -1)
-          .replace(/</g,"&lt;").replace(/>/g,"&gt;")
-          .replace(/(?:\\[rn])+/g, "<br />")
-          .replace(/\\\\/g, "\\");
-        this.addMessage(result);
+      if (!results || !results.results || !results.results.Result) {
+        this.addProcessing();
         this.updateTerminal();
+        setTimeout(() => { this.fetchResult() }, 5000);
+        return;
+      }
+      const result = results.results.Result.substr(1).slice(0, -1)
+        .replace(/</g, "&lt;").replace(/>/g, "&gt;")
+        .replace(/(?:\\[rn])+/g, "<br />")
+        .replace(/\\\\/g, "\\");
+      this.addMessage(result);
+      this.updateTerminal();
 
     }, () => {
-       
-    })  
+
+    })
   }
   title = 'app';
 }
